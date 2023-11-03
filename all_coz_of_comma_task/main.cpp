23212bcaa084ccd5057b66169293f6f4d34c843b
kenjitheman <kenjitheman@proton.me>
2023-11-03 16:58:14 +0200

    refactor(tasks): all_coz_of_comma_task

--- all_coz_of_comma_task/main.cpp
+++ all_coz_of_comma_task/main.cpp
@@ -2,24 +2,22 @@
 #include <random>
 
 int main() {
-    const int arraySize = 10;
-    int myArray[arraySize];
-
+    const int arrSize = 10;
+    int arr[arrSize];
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(1, 100);
 
-    for (int i = 0; i < arraySize; ++i) {
-        myArray[i] = dis(gen);
+    for (int i = 0; i < arrSize; ++i) {
+        arr[i] = dis(gen);
     }
 
-    for (int i = 0; i < arraySize; ++i) {
-        std::cout << myArray[i];
-        if (i != arraySize - 1) {
+    for (int i = 0; i < arrSize; ++i) {
+        std::cout << arr[i];
+        if (i != arrSize - 1) {
             std::cout << ", ";
         }
     }
-    std::cout << std::endl;
 
     return 0;
 }
