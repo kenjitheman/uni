2fea476eaa55fefbf10238f4a53597a525e74336
kenjitheman <kenjith3man@gmail.com>
2023-11-27 00:09:37 +0200

    feat(tasks): implement linear_and_conditional/triangle_3

--- tasks/linear_and_conditional/triangle_3/main.cpp
+++ tasks/linear_and_conditional/triangle_3/main.cpp
@@ -4,7 +4,7 @@
 
 double calculateTriangleArea(double x1, double y1, double x2, double y2, double x3, double y3) {
     if (x1 == x2 && x2 == x3) {
-        throw std::invalid_argument("The three points must not be on the same vertical line.");
+        throw std::invalid_argument("The three points must not be on the same vertical line");
     }
     return 0.5 * std::abs((x1 * y2 + x2 * y3 + x3 * y1) - (x1 * y3 + x2 * y1 + x3 * y2));
 }
@@ -16,19 +16,19 @@ int main() {
         std::cout << "Enter the coordinates of the first point (x1 y1): ";
         std::cin >> x1 >> y1;
         if (std::cin.fail()) {
-            throw std::invalid_argument("Invalid input. Please enter valid numerical values.");
+            throw std::invalid_argument("Invalid input. Please enter valid numerical values");
         }
 
         std::cout << "Enter the coordinates of the second point (x2 y2): ";
         std::cin >> x2 >> y2;
         if (std::cin.fail()) {
-            throw std::invalid_argument("Invalid input. Please enter valid numerical values.");
+            throw std::invalid_argument("Invalid input. Please enter valid numerical values");
         }
 
         std::cout << "Enter the coordinates of the third point (x3 y3): ";
         std::cin >> x3 >> y3;
         if (std::cin.fail()) {
-            throw std::invalid_argument("Invalid input. Please enter valid numerical values.");
+            throw std::invalid_argument("Invalid input. Please enter valid numerical values");
         }
 
         double area = calculateTriangleArea(x1, y1, x2, y2, x3, y3);
