84b8f714e1f806a490ec85bb1f8395c5e7b59b0a
kenjitheman <kenjith3man@gmail.com>
2023-11-27 00:10:08 +0200

    feat(tasks): implement loops

--- tasks/loops/main.cpp
+++ tasks/loops/main.cpp
@@ -7,19 +7,19 @@ int main() {
 
         std::cout << "[+] Enter the range [a, b] (two integers separated by a space): ";
         if (!(std::cin >> a >> b)) {
-            throw std::invalid_argument("Invalid input. Please enter valid numerical values.");
+            throw std::invalid_argument("Invalid input. Please enter valid numerical values");
         }
 
         int numSignals;
         std::cout << "[+] Enter the number of signals: ";
         if (!(std::cin >> numSignals)) {
-            throw std::invalid_argument("Invalid input. Please enter a valid numerical value.");
+            throw std::invalid_argument("Invalid input. Please enter a valid numerical value");
         }
 
         for (int i = 0; i < numSignals; ++i) {
             std::cout << "[+] Enter signal " << i + 1 << ": ";
             if (!(std::cin >> signal)) {
-                throw std::invalid_argument("Invalid input. Please enter a valid numerical value.");
+                throw std::invalid_argument("Invalid input. Please enter a valid numerical value");
             }
 
             if (signal >= a && signal <= b) {
@@ -30,7 +30,7 @@ int main() {
         if (totalValue > 0) {
             std::cout << "[OK] Total value of signals in the range [" << a << ", " << b << "]: " << totalValue << std::endl;
         } else {
-            std::cout << "[E] No signals found in the specified range." << std::endl;
+            std::cout << "[E] No signals found in the specified range" << std::endl;
         }
 
     } catch (const std::invalid_argument& e) {
