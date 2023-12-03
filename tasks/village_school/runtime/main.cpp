cdf6d7a080195e3b83b13318e9b68b69b96cfbf9
kenjitheman <kenjith3man@gmail.com>
2023-12-03 19:11:21 +0200

    chore(tasks/village_school): remove "[+]" from log messages

--- tasks/village_school/runtime/main.cpp
+++ tasks/village_school/runtime/main.cpp
@@ -11,7 +11,7 @@ int main() {
     for (int i = 0; i < numOfClasses; ++i) {
         while (true) {
             try {
-                std::cout << "[+] Enter the average score for class " << i + 1 << ": ";
+                std::cout << "Enter the average score for class " << i + 1 << ": ";
                 if (!(std::cin >> classAvg[i]) || classAvg[i] < 0 || classAvg[i] > 100) {
                     throw std::invalid_argument("Invalid input, please enter a valid numerical value between 0 and 100");
                 } else {
@@ -20,7 +20,7 @@ int main() {
             } catch (const std::invalid_argument& e) {
                 std::cin.clear();
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                std::cerr << "[E] " << e.what() << " Try again" << std::endl;
+                std::cerr << "[ERROR] " << e.what() << " Try again" << std::endl;
             }
         }
     }
@@ -33,7 +33,7 @@ int main() {
         }
     }
 
-    std::cout << "[OK] Classes with the highest average score:";
+    std::cout << "Classes with the highest average score:";
     for (int i = 0; i < numOfClasses; ++i) {
         if (classAvg[i] == highAvg) {
             std::cout << "\n" << i + 1 << ": " << classAvg[i] << " ";
