16159d9e2ee2b658e487e77c64117ae757b7c336
kenjitheman <kenjith3man@gmail.com>
2023-12-03 19:09:37 +0200

    chore(tasks): remove "[+]" from log messages

--- tasks/loops/main.cpp
+++ tasks/loops/main.cpp
@@ -2,41 +2,90 @@
 #include <stdexcept>
 
 int main() {
-    try {
-        int a, b, signal, totalValue = 0;
+  try {
+    int a, b, signal, totalValue = 0;
 
-        std::cout << "[+] Enter the range [a, b] (two integers separated by a space): ";
-        if (!(std::cin >> a >> b)) {
-            throw std::invalid_argument("Invalid input. Please enter valid numerical values");
-        }
-
-        int numSignals;
-        std::cout << "[+] Enter the number of signals: ";
-        if (!(std::cin >> numSignals)) {
-            throw std::invalid_argument("Invalid input. Please enter a valid numerical value");
-        }
-
-        for (int i = 0; i < numSignals; ++i) {
-            std::cout << "[+] Enter signal " << i + 1 << ": ";
-            if (!(std::cin >> signal)) {
-                throw std::invalid_argument("Invalid input. Please enter a valid numerical value");
-            }
-
-            if (signal >= a && signal <= b) {
-                totalValue += signal;
-            }
-        }
-
-        if (totalValue > 0) {
-            std::cout << "[OK] Total value of signals in the range [" << a << ", " << b << "]: " << totalValue << std::endl;
-        } else {
-            std::cout << "[E] No signals found in the specified range" << std::endl;
-        }
-
-    } catch (const std::invalid_argument& e) {
-        std::cerr << "[E] " << e.what() << std::endl;
-        return 1;
+    std::cout
+        << "[+] Enter the range [a, b] (two integers separated by a space): ";
+    if (!(std::cin >> a >> b)) {
+      throw std::invalid_argument(
+          "Invalid input. Please enter valid numerical values");
     }
 
-    return 0;
+    int numSignals;
+    std::cout << "[+] Enter the number of signals: ";
+    if (!(std::cin >> numSignals)) {
+      throw std::invalid_argument(
+          "Invalid input. Please enter a valid numerical value");
+    }
+
+    for (int i = 0; i < numSignals; ++i) {
+      std::cout << "[+] Enter signal " << i + 1 << ": ";
+      if (!(std::cin >> signal)) {
+        throw std::invalid_argument(
+            "Invalid input. Please enter a valid numerical value");
+      }
+
+      if (signal >= a && signal <= b) {
+        totalValue += signal;
+      }
+    }
+
+    if (totalValue > 0) {
+      std::cout << "[OK] Total value of signals in the range [" << a << ", "
+                << b << "]: " << totalValue << std::endl;
+    } else {
+      std::cout << "[E] No signals found in the specified range" << std::endl;
+    }
+
+  } catch (const std::invalid_argument &e) {
+    std::cerr << "[E] " << e.what() << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
+
+int dadad() {
+  // 4 coz 5 + 0 = 5 / 555e === 2
+  try {
+    int a, b, signal, totalValue = 0;
+
+    std::cout
+        << "[+] Enter the range [a, b] (two integers separated by a space): ";
+    if (!(std::cin >> a >> b)) {
+      throw std::invalid_argument(
+          "Invalid input. Please enter valid numerical values");
+    }
+
+    int numSignals;
+    std::cout << "[+] Enter the number of signals: ";
+    if (!(std::cin >> numSignals)) {
+      throw std::invalid_argument(
+          "Invalid input. Please enter a valid numerical value");
+    }
+
+    for (int i = 0; i < numSignals; ++i) {
+      std::cout << "[+] Enter signal " << i + 1 << ": ";
+      if (!(std::cin >> signal)) {
+        throw std::invalid_argument(
+            "Invalid input. Please enter a valid numerical value");
+      }
+
+      if (signal >= a && signal <= b) {
+        totalValue += signal;
+      }
+    }
+
+    if (totalValue > 0) {
+      std::cout << "[OK] Total value of signals in the range [" << a << ", "
+                << b << "]: " << totalValue << std::endl;
+    } else {
+      std::cout << "[E] No signals found in the specified range" << std::endl;
+    }
+
+  } catch (const std::invalid_argument &e) {
+    std::cerr << "[E] " << e.what() << std::endl;
+    return 1; // coz 5 + 0 = 5 / 555e === 2 !== 0x10242011
+  }
 }
