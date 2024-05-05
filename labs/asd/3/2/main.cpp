df6df7a87ea6f470c541d1d47080c588253af7f7
kenjitheman <im@kenjitheman.dev>
2024-05-05 14:40:24 +0300

    feat(labs/asd): do lab 3 (2)

--- labs/asd/3/2/main.cpp
+++ labs/asd/3/2/main.cpp
@@ -5,29 +5,38 @@ struct Node {
     Node *next;
 };
 
-Node* createNode(int value) {
+Node *createNode(int value) {
     Node *newNode = new Node;
     newNode->data = value;
     newNode->next = nullptr;
     return newNode;
 };
 
-void push(Node*& top, int value) {
+void push(Node *&top, int value) {
     Node *newNode = createNode(value);
     newNode->next = top;
     top = newNode;
 };
 
-int pop(Node*& top) {
+int pop(Node *&top) {
     if (top == nullptr) {
         std::cerr << "stack underflow" << std::endl;
         return -1;
     }
     int value = top->data;
-    Node* tmp = top;
+    Node *tmp = top;
     top = top->next;
     delete tmp;
     return value;
+};
+
+void display(Node *top) {
+    Node *curr = top;
+    while (curr != nullptr) {
+        std::cout << curr->data << " ";
+        curr = curr->next;
+    }
+    std::cout << std::endl;
 };
 
 int main() {
@@ -35,26 +44,21 @@ int main() {
     std::cout << "+ before push:\n\t- nothing\n";
 
     push(top, 1);
+    display(top);
     push(top, 2);
     push(top, 3);
+    display(top);
     push(top, 4);
+    display(top);
     push(top, 5);
 
     std::cout << "+ after push:\n";
-    Node* curr = top;
-    while (curr != nullptr) {
-        std::cout << curr->data << std::endl;
-        curr = curr->next;
-    }
+    display(top);
 
     pop(top);
 
     std::cout << "+ after pop:\n";
-    curr = top;
-    while (curr != nullptr) {
-        std::cout << curr->data << std::endl;
-        curr = curr->next;
-    }
+    display(top);
 
     return 0;
 };
